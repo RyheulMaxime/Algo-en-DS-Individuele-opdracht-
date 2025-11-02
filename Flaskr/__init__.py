36bfb6de3a68c4ecd2f164e9e6e63e750b39b7a5
Maxime Ryheul <maxime.ryheul@student.howest.be>
2025-11-02 17:52:03 +0100

    Added first instance of Kanban board and some side files still to fill in

--- Flaskr/__init__.py
+++ Flaskr/__init__.py
@@ -1,15 +1,19 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from .config import Config
+# from .db import db
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    # )
+    app.config.from_object(Config)
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -25,8 +29,13 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    # @app.route('/hello')
+    # def hello():
+    #     return 'Hello, World!'
+
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+        # return 'Test'
 
     return app
